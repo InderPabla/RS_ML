653b6fb9d6b332165f36b23ecf621d00f95cf92b
Inderpreet Pabla <inderpreet.pabla@gmail.com>
2017-09-13 21:59:09 -0400

    combo macro appen

--- src/MacroPlayer.java
+++ src/MacroPlayer.java
@@ -1,9 +1,10 @@
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class MacroPlayer {
 
-	ArrayList<MacroTask> macroTaskList;
-	MacroTask currentTask = null;
+	private ArrayList<MacroTask> macroTaskList;
+	public MacroTask currentTask = null;
 	
 	public MacroPlayer() {
 		macroTaskList = new ArrayList<MacroTask>();
@@ -11,12 +12,35 @@ public class MacroPlayer {
 	
 	public void macroTick() {
 		if(currentTask!=null) {
-			
+			if(currentTask.isMacroPlay() == true) {
+				MacroAction action = currentTask.getAction();
+				currentTask = null;
+			}
 		}
 	}
 	
 	public void clearMacroTask() {
 		macroTaskList = new ArrayList<MacroTask>();
+		currentTask = null;
+	}
+	
+	public void appenComboMacro() {
+		macroTaskList.add(new MacroTask(0,MacroAction.MOVE));
+		macroTaskList.add(new MacroTask(10,MacroAction.PRESS));
+		macroTaskList.add(new MacroTask(5,MacroAction.RELEASE));
+		macroTaskList.add(new MacroTask(10,MacroAction.MOVE));
+		macroTaskList.add(new MacroTask(20,MacroAction.PRESS));
+		macroTaskList.add(new MacroTask(5,MacroAction.RELEASE));
+		macroTaskList.add(new MacroTask(1600,MacroAction.MOVE));
+		macroTaskList.add(new MacroTask(10,MacroAction.PRESS));
+		macroTaskList.add(new MacroTask(5,MacroAction.RELEASE));
+		macroTaskList.add(new MacroTask(20,MacroAction.MOVE));
+		macroTaskList.add(new MacroTask(5,MacroAction.PRESS));
+		macroTaskList.add(new MacroTask(5,MacroAction.RELEASE));
+	}
+	
+	public void appendEatMacro() {
+		
 	}
 
 }
--- src/MacroTask.java
+++ src/MacroTask.java
@@ -1,12 +1,38 @@
 
 public class MacroTask {
 
-	long taskWait;
-	MacroAction action;
+	
+	private MacroAction action;
+	
+	private boolean activateMacro;
+	
+	private long taskWait;
+	private long macroEndTime;
 	
 	public MacroTask(long taskWait,MacroAction action) {
+		this.taskWait = taskWait;
+		this.action = action;
 		
+		activateMacro = false;
 	}
-
 	
+	//Macro becomes the current macro
+	public void activateMacro() {
+		activateMacro = true;
+		macroEndTime = System.currentTimeMillis();
+	}
+	
+	//macro tick
+	public boolean isMacroPlay() {
+		long currentTime = System.currentTimeMillis();
+		if(currentTime>macroEndTime) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public MacroAction getAction() {
+		return action;
+	}
 }
